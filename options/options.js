d7b50294bcedfa3ff231253e22dc1e1ea185f8d0
Crowdbotics <team@crowdbotics.com>
2023-02-21 00:23:48 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "shiny-feather-39045",
+  url: "https://shiny-feather-39045.botics.co",
+  api: "https://shiny-feather-39045.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
